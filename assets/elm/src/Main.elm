1e94165809a0e7af3e238be0e06148e8cca04233
Ruslan Kenzhebekov <rkenzheb@pinc.com>
2018-01-28 20:11:18 +0600

    Submit form fields using elm

--- assets/elm/src/Main.elm
+++ assets/elm/src/Main.elm
@@ -2,6 +2,25 @@ module Main exposing (main)
 
 import Html exposing (Html)
 
-main : Html msg
+import Messages exposing (Msg(..))
+import Model exposing (..)
+import Update exposing (update)
+import View exposing (view)
+
+
+main : Program Never Model Msg
 main =
-  Html.text "Hello, Elm!"
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+init : ( Model, Cmd Msg )
+init =
+    initialModel ! []
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
